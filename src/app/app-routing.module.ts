1b4481bc9accf7724dbb587850a7e034e6eb3066
Adedamola Ajasa <azeemajasa@gmail.com>
2023-04-27 15:37:13 +0100

    initialization of app components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FlightsComponent } from './flights/flights.component';
+import { StatesComponent } from './states/states.component';
+import { TracksComponent } from './tracks/tracks.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'flights', component: FlightsComponent},
+  { path: 'states', component: StatesComponent},
+  { path: 'tracks', component: TracksComponent},
+
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { StatesComponent } from './states/states.component';
+import { FlightsComponent } from './flights/flights.component';
+import { TracksComponent } from './tracks/tracks.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    StatesComponent,
+    FlightsComponent,
+    TracksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
